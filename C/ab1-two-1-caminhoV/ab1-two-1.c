e61f11a4eef8b1dfe18d774771d51612f58b4470
mawabi-oliveira <poopys.al.br@gmail.com>
2026-09-18 19:18:54 -0300

    adicionando novo codigo

--- C/ab1-two-1-caminhoV/ab1-two-1.c
+++ C/ab1-two-1-caminhoV/ab1-two-1.c
@@ -2,60 +2,45 @@
 #include <string.h>
 #include <math.h>
 #include <stdlib.h>
-#include <stdio.h>
 
 void doryr(int n, int k, int e1, int e2, int e3, int e4, int e5, int pos, int m, int dist, int dir) {
-    int c = 0;
-    
+    int c;
     if (m == 0) c = e1;
     else if (m == 1) c = e2;
     else if (m == 2) c = e3;
     else if (m == 3) c = e4;
-    else if (m == 4) c = e5;
+    else c = e5;
 
     if (m == k) {
         if (pos == n) {
             printf("Destino alcancado, distancia percorrida: %d\n", dist);
             return;
         }
-
-        pos++;
-        dist++;
-        printf("Indo, posicao %d\n", pos);
-        doryr(n, k, e1, e2, e3, e4, e5, pos, m, dist, 1); //
+        printf("Indo, posicao %d\n", ++pos);
+        doryr(n, k, e1, e2, e3, e4, e5, pos, m, ++dist, 1);
         return;
     }
 
-    //
     if (dir == 1) {
         if (pos < c) {
-            pos++;
-            dist++;
-            printf("Indo, posicao %d\n", pos);
-            doryr(n, k, e1, e2, e3, e4, e5, pos, m, dist, 1);
+            printf("Indo, posicao %d\n", ++pos);
+            doryr(n, k, e1, e2, e3, e4, e5, pos, m, ++dist, 1);
             return;
         }
-        
         if (pos == c) {
-            pos--;
-            dist++;
-            printf("Voltando, posicao %d\n", pos);
-            doryr(n, k, e1, e2, e3, e4, e5, pos, m, dist, -1);
+            printf("Voltando, posicao %d\n", --pos);
+            doryr(n, k, e1, e2, e3, e4, e5, pos, m, ++dist, -1);
             return;
         }
     } 
-
     else {
         if (pos > 0) {
-            pos--;
-            dist++;
-            printf("Voltando, posicao %d\n", pos);
-            doryr(n, k, e1, e2, e3, e4, e5, pos, m, dist, -1);
+            printf("Voltando, posicao %d\n", --pos);
+            doryr(n, k, e1, e2, e3, e4, e5, pos, m, ++dist, -1);
             return;
         }
         if (pos == 0) {
-            m++; 
-            doryr(n, k, e1, e2, e3, e4, e5, pos, m, dist, 1);
+            doryr(n, k, e1, e2, e3, e4, e5, pos, ++m, dist, 1);
             return;
         }
     }
@@ -68,21 +53,11 @@ int main() {
     scanf("%d", &n);
     scanf("%d", &k);
 
-    if (k > 0) {
-        scanf("%d", &e1);
-        if (k > 1) {
-            scanf("%d", &e2);
-            if (k > 2) {
-                scanf("%d", &e3);
-                if (k > 3) {
-                    scanf("%d", &e4);
-                    if (k > 4) {
-                        scanf("%d", &e5);
-                    }
-                }
-            }
-        }
-    }
+    if (k > 0) scanf("%d", &e1);
+    if (k > 1) scanf("%d", &e2);
+    if (k > 2) scanf("%d", &e3);
+    if (k > 3) scanf("%d", &e4);
+    if (k > 4) scanf("%d", &e5);
 
     doryr(n, k, e1, e2, e3, e4, e5, 0, 0, 0, 1);
 
--- C/ab1-two-rafC/ab1-two.c
+++ C/ab1-two-rafC/ab1-two.c
@@ -1,17 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
 int album(int n, int ano_nasc)
 {
     int a, t;
     if (n == 0)
         return 1;
-    else
-    {
-        scanf("%d %d", &a, &t);
-        if (a - t == ano_nasc)
-            return album(n - 1, ano_nasc);
-        else
-            return 0;
-    }
+    scanf("%d %d", &a, &t);
+    return (a - t == ano_nasc) && album(n - 1, ano_nasc);
 }
+
 int main()
 {
     int n, a, t;
@@ -23,6 +23,4 @@ int main()
         printf("mentiu a idade\n");
     return 0;
 }
-
-
 
